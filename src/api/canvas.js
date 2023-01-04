7bcbbc25c93d3617bcd474c6c46a5c9044637dae
Suphakit P <me@suphakit.net>
2023-01-05 03:36:44 +0700

    added: canvas api Frame

---
+++ src/api/canvas.js
@@ -0,0 +1,14 @@
+// Module Call
+let { createCanvas, loadImage } = require('canvas')
+
+// Variable Call
+let width = 1280, height = 720;
+let canvas = createCanvas(width, height)
+
+async function canvasSummon(req, res) {
+
+}
+
+module.exports = {
+  canvasSummon
+}
--- src/page.js
+++ src/page.js
@@ -1,6 +1,13 @@
+// SRC Call
+let cc = require('./api/canvas')
+
 function pListing(app) {
   app.get('/', (req, res) => {
     res.send('a')
+  })
+
+  app.get('/api/', (req, res) => {
+    cc.canvasSummon(req, res)
   })
 }
 
