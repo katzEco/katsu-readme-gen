8c2d90facc4b4a1bb8b0eff21b21e619753a6bd3
Suphakit P <me@suphakit.net>
2023-01-08 03:02:51 +0700

    added: askMeAbout function

--- src/api/appSplit/ask.js
+++ src/api/appSplit/ask.js
@@ -2,7 +2,16 @@ let check = require('./check')
 
 async function ask(req) {
   let askMeAbout = ''
-  console.log(req.query);
+
+  req.query.asks.forEach(ask => {
+    askMeAbout = check.check(askMeAbout) + '"' + ask + '"'
+  });
+
+  askMeAbout = '[' + askMeAbout + ']'
 
   return askMeAbout
+}
+
+module.exports = {
+  ask
 }
