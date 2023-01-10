81735807d5429e67b5b178fba01632e87bac421e
Suphakit P <me@suphakit.net>
2023-01-11 00:46:18 +0700

    updated: component

--- src/api/appSplit/otherComponent.js
+++ src/api/appSplit/otherComponent.js
@@ -4,6 +4,7 @@ async function comp(ghUserName) {
   let statHead = '# My Stats'
   let statBody = `${cnt.cent(`<a href="https://github.com/ryo-ma/github-profile-trophy"><img src="https://github-profile-trophy.vercel.app/?username=${ghUserName}&theme=onedark&title=MultiLanguage,Stars,Commit,Followers,Issues,Repositories" alt="dethmastery" align="center" /></a>`)}
 ${cnt.cent(`<a href="https://stat-card.000198.xyz"><img src="https://stat-card.000198.xyz/api?username=${ghUserName}&count_private=true&show_icons=true&theme=panda" alt="statCard" /></a>`)}
+${cnt.cent(`<a href="https://stat-card.000198.xyz"><img src="https://stat-card.000198.xyz/api/top-langs/?username=${ghUserName}&layout=compact&theme=panda" alt="statCard" /></a>`)}
   `
 
   let final = `${statHead}
