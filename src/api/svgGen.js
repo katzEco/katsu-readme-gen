da47077d0e1342d3b00f91b9928bed3c267f8e8e
Suphakit P <me@suphakit.net>
2023-01-12 02:12:44 +0700

    updated: finished SVG GEN

--- src/api/svgGen.js
+++ src/api/svgGen.js
@@ -12,6 +12,10 @@ async function gen(req, res) {
   let width = 1280
   let height = 390
 
+  let date = new Date()
+  let nYear = date.getFullYear()
+  let age = nYear - query.bYear
+
   let data = `
   <style>
     @import url('https://cdn.katsuragi.cyou/fonts/TH/LINEseed-Regular');
@@ -35,14 +39,25 @@ async function gen(req, res) {
   </g>
 
   <g id="textBox">
-    <text x="40" y="60" width="500" height="200" font-family="LINEseed-Regular, Calibri" style="fill: #${query.fg}; font-size: 24px;">
+    <text x="50" y="60" width="500" height="200" font-family="LINEseed-Regular, Calibri" style="fill: #${query.fg}; font-size: 24px;">
       Github : ${query.gh}
     </text>
+    <text x="70" y="130" width="480" height="200" font-family="LINEseed-Regular, Calibri" style="fill: #${query.fg}; font-size: 75px;">
+      ${query.name} ${query.lName}
+    </text>
+    <text x="90" y="180" width="480" height="150" font-family="LINEseed-Regular, Calibri" style="fill: #${query.fg}; font-size: 20px;">
+      ${query.subText}
+    </text>
+    <foreignObject x="${width - ((width/2) + 100)}" y="${height - (50 + 90)}" width="${width/2 - (100 + 150)}" height="150" text-anchor="end">
+      <p xmlns="http://www.w3.org/1999/xhtml" style="color: #${query.fg}; font-size: 20px; font-family: 'LINEseed-Regular', 'Calibri'; text-align: right;">
+        ${query.ageLeft} ${age} ${query.ageRight}
+      </p>
+    </foreignObject>
   </g>
 
   <g id="badgeBox">
-    <image href="https://img.shields.io/twitter/follow/${query.tw}?logo=twitter&style=for-the-badge" x="40" y="${height - (40 + 25)}" />
-    <image href="https://komarev.com/ghpvc/?username=${query.gh}&label=Profile%20views&color=0e75b6&style=for-the-badge" x="40" y="${height - (40 + 60)}" />
+    <image href="https://img.shields.io/twitter/follow/${query.tw}?logo=twitter&style=for-the-badge" x="50" y="${height - (50 + 25)}" />
+    <image href="https://komarev.com/ghpvc/?username=${query.gh}&label=Profile%20views&color=0e75b6&style=for-the-badge" x="50" y="${height - (50 + 65)}" />
   </g>
 `
 
